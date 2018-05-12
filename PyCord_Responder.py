a3136be88dea757a3b0ef30f49835bfcb229a89c
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2018-05-12 13:05:11 +0100

    added game changing

--- PyCord_Responder.py
+++ PyCord_Responder.py
@@ -4,14 +4,19 @@ import discord
 import asyncio
 import random
 
+
+
 bot = discord.Client()
 
 @bot.event
 async def on_ready():
 		print("Logged in as", bot.user.name)
+		game = input("What shall the playing message be? (say none for no game) ")
+		if game != "none":
+			await bot.change_presence(game=discord.Game(name=game))
 		while True :
 			msg = input("Message: ")
-			await bot.send_message(discord.Object(id='channelid'), msg)
+			await bot.send_message(discord.Object(id='channel id'), msg)
 
 
 
