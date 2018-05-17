badfe4b622c483549986411829ee55f3b0c0e31f
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2018-05-17 21:50:07 +0100

    added multiuser support for multiple people using the same bot

--- PyCord_Responder.py
+++ PyCord_Responder.py
@@ -11,12 +11,13 @@ bot = discord.Client()
 @bot.event
 async def on_ready():
 		print("Logged in as", bot.user.name)
-		game = input("What shall the playing message be? (say none for no game) ")
-		if game != "none":
-			await bot.change_presence(game=discord.Game(name=game))
+		username = input("Username: ")
+		await bot.change_presence(game=discord.Game(name=username + " is using Pycord"))
+
 		while True :
 			msg = input("Message: ")
-			await bot.send_message(discord.Object(id='channel id'), msg)
+			#replace the "channel id" with the id of the channel you want to talk in
+			await bot.send_message(discord.Object(id='channel id'), "`[" + username + "]` " + msg)
 
 
 
@@ -25,5 +26,5 @@ async def on_ready():
 
 
 
-
+#replace the token with a bot token or bot.run("email","password") for user accounts
 bot.run("token")
