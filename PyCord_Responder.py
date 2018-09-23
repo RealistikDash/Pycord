0f3a064963e99dcdc50c90e98eef42ae37774d4a
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2018-09-23 14:04:32 +0200

    Started using .format instead of +
    cleaner code i guess..............................

--- PyCord_Responder.py
+++ PyCord_Responder.py
@@ -1,4 +1,5 @@
 #PyCord Responder by RealistikDash
+#From https://github.com/RealistikDash/PyCord
 print("Initialising Pycord Responder")
 
 #All the necessary imports
@@ -33,8 +34,8 @@ bot = discord.Client()
 
 @bot.event
 async def on_ready():
-		print("Logged in as", bot.user.name)
-		username = input("Enter a username: ")
+		print("[Pycord Info] Logged into", bot.user.name)
+		username = input("[Pycord] Enter a username: ")
 		await bot.change_presence(game=discord.Game(name=username + " is using Pycord"))
 		#Welcome message
 		print("Welcome to Pycord {}!".format(username))
@@ -53,35 +54,35 @@ async def on_ready():
 			elif msg.startswith("/setgame"):
 				game = msg[9:]
 				await bot.change_presence(game=discord.Game(name=game))
-				print("[Pycord Info] Game set to " + msg[9:])
+				print("[Pycord Info] Game set to {}".format(msg[9:]))
 			#Send file command (Use: /sendfile file.png), all formats supported as long as it is under 8MB
 			elif msg.startswith("/sendfile"):
-				print("[Pycord Info] Sending file...")
+				print("[Pycord Info] Sending file {}...".format(msg[10:]))
 				await bot.send_file(discord.Object(id=channelId), msg[10:])
 			#Lets the user set the parameters for the send
 			elif msg.startswith("/customsend"):
 				print("[Pycord Info] Attempting custom send...")
-				eval("""await bot.send_message(""" + msg[12:] + ")")
+				eval("""await bot.send_message({})""".format(msg[12:]))
 			#Changes pycord username
 			elif msg.startswith("/changeuser"):
 				username = msg[12:]
-				print("[Pycord Info] Username changed to " + username)
+				print("[Pycord Info] Username changed to {}".format(username))
 			#Import custom modules for use with /customsend
 			elif msg.startswith("/python.import"):
-				eval("import " + msg[15:])
+				eval("import {}".format(msg[15:]))
 				print("[Pycord Info] Import attempted. Successful unless given an error.")
 			#Opens a text file and says it's contents in discord
 			elif msg.startswith("/sendtxt"):
 				file = open(msg[9:], 'r')
 				print("[Pycord Info] Sending txt file...")
-				await bot.send_message(discord.Object(id=channelId), "`[" + username + """]` TXT FILE 
-""" + file.read())
+				await bot.send_message(discord.Object(id=channelId), """`[{}]` TXT FILE 
+""".format(username) + file.read())
 			#Change channel ID
 			elif msg.startswith("/changeid"):
 				channelId = msg[10:]
 			#If none of the requirements above are met, send the message
 			else:
-				await bot.send_message(discord.Object(id=channelId), "`[" + username + "]` " + msg)
+				await bot.send_message(discord.Object(id=channelId), "`[{}]` {}".format(username, msg))
 
 
 bot.run(botToken) #Connects the bot.
