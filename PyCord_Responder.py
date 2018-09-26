a411df67f037bd7d0f436fa31f42c5564b52accb
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2018-09-26 17:50:47 +0200

    Added /shrug, made code more readable and more comments
    dadadadadadada

--- PyCord_Responder.py
+++ PyCord_Responder.py
@@ -23,6 +23,7 @@ Discord Based Commands
 -/changeuser		Changes your Pycord username.
 -/sendtxt		Sends the contents of a specified txt file.
 -/changeid		Changes the channel id to which your messages are being sent to.
+-/shrug			Sends a ¯\_(ツ)_/¯
 -/exit			Exits Pycord"""
 
 
@@ -53,50 +54,64 @@ async def on_ready():
 		print("---------------------------------------------------------------------")
 		######################################
 
-		while True :
+		while True : #The main loop
 			msg = input("Pycord> ")
 			#Checks if the msg is empty
 			if msg == "":
 				print("[Pycord Error] Cannot send empty message.")
+
 			#Changes the presence
 			elif msg.startswith("/setgame"):
 				game = msg[9:]
 				await bot.change_presence(game=discord.Game(name=game))
 				print("[Pycord Info] Game set to {}".format(msg[9:]))
+
 			#Send file command (Use: /sendfile file.png), all formats supported as long as it is under 8MB
 			elif msg.startswith("/sendfile"):
 				print("[Pycord Info] Sending file {}...".format(msg[10:]))
 				await bot.send_file(discord.Object(id=channelId), msg[10:])
+
 			#Lets the user set the parameters for the send
 			elif msg.startswith("/customsend"):
 				print("[Pycord Info] Attempting custom send...")
 				eval("""await bot.send_message({})""".format(msg[12:]))
+
 			#Changes pycord username
 			elif msg.startswith("/changeuser"):
 				username = msg[12:]
 				print("[Pycord Info] Username changed to {}".format(username))
+
 			#Import custom modules for use with /customsend
 			elif msg.startswith("/python.import"):
 				eval("import {}".format(msg[15:]))
 				print("[Pycord Info] Import attempted. Successful unless given an error.")
+
 			#Opens a text file and says it's contents in discord
 			elif msg.startswith("/sendtxt"):
 				file = open(msg[9:], 'r')
 				print("[Pycord Info] Sending txt file...")
 				await bot.send_message(discord.Object(id=channelId), """`[{}]` TXT FILE 
 """.format(username) + file.read())
+			
 			#Change channel ID
 			elif msg.startswith("/changeid"):
 				channelId = msg[10:]
+
 			#Lists all the commands
 			elif msg.startswith("/help"):
 				print(cmdlist)
+
 			#Exits PyCord
 			elif msg.startswith("/exit"):
 				print(exitMsg)
 				time.sleep(3)
-				await bot.close()
-				break
+				await bot.close() #Logs out
+				break #Breaks the loop
+			
+			#Sends the shrug emote
+			elif msg.startswith("/shrug"):
+				await bot.send_message(discord.Object(id=channelId), "`[{}]` ¯\_(ツ)_/¯".format(username))
+
 			#If none of the requirements above are met, send the message
 			else:
 				await bot.send_message(discord.Object(id=channelId), "`[{}]` {}".format(username, msg))
