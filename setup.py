206b7c151c37e2e67ade46b1c72ba6d24b5b6ea3
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2018-10-02 13:43:04 +0100

    Finished setup

--- setup.py
+++ setup.py
@@ -1,3 +1,34 @@
 #Pycord Setup
 #Imports
 from Pycord import moduleManagement as mm
+from Pycord import pycord
+import time
+###########################################
+
+
+#Welcome
+print("-----------------------------------------")
+print("Welcome to the Pycord setup.")
+print("-----------------------------------------")
+print("")
+x = input("Do you want to start the setup (y/N)? ")
+x = x.lower() #Changes x into lower case
+if x == "y":
+  pycord.log("Starting to install modules...")
+  mm.installModule("-r requirements.txt")
+  pycord.log("Installing modules done!")
+  time.sleep(1)
+  pycord.log("Building modules for quicker imports...")
+  import discord
+  import requests
+  import aiohttp
+  pycord.log("Done!")
+  pycord.log("Setup complete!")
+  print("-----------------------------------------")
+  print("The Pycord setup was completed!")
+  print("Exiting in 5 seconds...")
+  print("-----------------------------------------")
+  time.sleep(5)
+  pycord.log("Exitting...")
+  exit()
+  
