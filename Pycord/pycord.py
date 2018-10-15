4d56defd32dc49c5abba27925ed5ed77e8d8d403
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2018-10-15 22:55:02 +0200

    colorama test

--- Pycord/pycord.py
+++ Pycord/pycord.py
@@ -1,11 +1,20 @@
 #The Pycord module.
 
+#imports
+from colorama import Fore, Back, Style
+from colorama import init
+#################
+
+init()
+
 
 #Loggers
 def log(say): #Simple logger for quick modifications
     """Logs something with the Pycord info tag"""
-    print('[Pycord Log]', say)
+    print(Fore.YELLOW + '[Pycord Log]', say)
+    print(Fore.RESET)
 
 def errorLog(error): #Simple error logger for quick modifications
     """Throws an error of choice with the Pycord tag"""
-    print('[Pycord Error]', error)
+    print(Fore.RED + '[Pycord Error]', error)
+    print(Fore.RESET)
