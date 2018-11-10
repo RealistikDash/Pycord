93c1d2611ab143f2967cedcb1d0bfc0655f6ebd9
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2018-11-10 01:59:00 +0100

    Fixed checkNumber()

--- Pycord/pycord.py
+++ Pycord/pycord.py
@@ -21,7 +21,7 @@ def checkNumber(variable):
     """Checks if a given variable only consists of numbers"""
     try: #Attempts to turn the variable into and int
         variable = int(variable)
-        return true
+        return 'true'
     
     except Exception: #if it fails, false is returned
-        return false
+        return 'false'
