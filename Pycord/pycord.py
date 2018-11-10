2adb657265bdd4e9be37e51ffc4557575aaf011e
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2018-11-10 01:54:24 +0100

    Fixed error formatting

--- Pycord/pycord.py
+++ Pycord/pycord.py
@@ -11,13 +11,11 @@ init()
 #Loggers
 def log(say): #Simple logger for quick modifications
     """Logs something with the Pycord info tag"""
-    print(Fore.YELLOW + '[Pycord Log]', say)
-    print(Fore.RESET)
+    print(Fore.YELLOW + '[Pycord Log]', say, Fore.RESET)
 
 def errorLog(error): #Simple error logger for quick modifications
     """Throws an error of choice with the Pycord tag"""
-    print(Fore.RED + '[Pycord Error]', error)
-    print(Fore.RESET)
+    print(Fore.RED + '[Pycord Error]', error, Fore.RESET)
 
 def checkNumber(variable):
     """Checks if a given variable only consists of numbers"""
