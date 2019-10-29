259fd688ed64d8416fca12a052aefc31c17eb3e9
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2019-10-29 19:20:54 +0000

    Added colours (Mac, Linux and Experimental mode only)

--- basicFunc.py
+++ basicFunc.py
@@ -1,6 +1,37 @@
 #Another global module. This one is for modules.
 import time
 import asyncio
+import platform
+from config import *
+
+class colour:
+    #All this mess is because they dont show well at all on some systems. This is a basic estimate of what system this will work on
+    PermittedOS = ["Linux", "Darwin"]
+    CurrentOS = platform.system()
+    if CurrentOS in PermittedOS or settings["ExperimantalMode"] == 1:
+        PURPLE = "\033[95m"
+        CYAN = "\033[96m"
+        DARKCYAN = "\033[36m"
+        BLUE = "\033[94m"
+        GREEN = "\033[92m"
+        YELLOW = "\033[93m"
+        RED = "\033[91m"
+        BOLD = "\033[1m"
+        UNDERLINE = "\033[4m"
+        NORMAL = "\033[0m"
+    else:
+        PURPLE = ""
+        CYAN = ""
+        DARKCYAN = ""
+        BLUE = ""
+        GREEN = ""
+        YELLOW = ""
+        RED = ""
+        BOLD = ""
+        UNDERLINE = ""
+        NORMAL = ""
+
+
 
 async def DateFormat(messageFormat, message):
     """Adds date etc to the line. Currently reserved for later"""
@@ -8,11 +39,11 @@ async def DateFormat(messageFormat, message):
 
 def ErrorMessage(message):
     """Template for error messages (not finished)"""
-    print("An error has occured with Pycord!\nError: {}".format(message))
+    print("{}An error has occured with Pycord!\nError: {}{}".format(colour.YELLOW, message, colour.NORMAL))
 
 async def AsyncErrorMessage(message):
     """Template for error messages. But guess what? It's asyncronous (not finished)"""
-    print("An error has occured with Pycord!\nError: {}".format(message))
+    print("{}An error has occured with Pycord!\nError: {}{}".format(colour.YELLOW, message, colour.NORMAL))
 
 def ConfigIntegrityCheck(config):
     """Checks the integrity of the config and whether or not it will crash Pycord"""
@@ -20,6 +51,7 @@ def ConfigIntegrityCheck(config):
         try:
             #only real way of trying i know
             coolVariable = config["token"]
+            coolVariable = config["ExperimantalMode"]
             coolVariable = config["Messenger"]
             if type(coolVariable) is dict:
                 int(coolVariable["channelId"])
