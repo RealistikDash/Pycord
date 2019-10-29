f7aa283ee8768137b3c5d2bba770d64793aa6315
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2019-10-29 23:30:11 +0000

    Moved LoadingBar to basicFunc

--- basicFunc.py
+++ basicFunc.py
@@ -3,6 +3,18 @@ import time
 import asyncio
 import platform
 from config import *
+import sys
+
+#THE loading bar
+def LoadingBar(value, endvalue, bar_length=20):
+    """Loading bar cool right!"""
+
+    percent = float(value) / endvalue #makes it percentage certified
+    arrow = '-' * int(round(percent * bar_length)-1) + '>'
+    spaces = ' ' * (bar_length - len(arrow))
+
+    sys.stdout.write("\r[{0}] {1}%".format(arrow + spaces, int(round(percent * 100))))
+    sys.stdout.flush()
 
 class colour:
     #All this mess is because they dont show well at all on some systems. This is a basic estimate of what system this will work on
