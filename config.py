53d7b50be69f079ec07573df9553be99fd8a1e17
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2019-10-29 19:18:42 +0000

    Added ExperimentalMode Toggle

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
 #This is the Pycord Rewrite config file. This is used by both Messenger and Viewer
 settings = {
     "token" : "",
+    "ExperimantalMode" : 0, #enables features that might not be working well
     "Messenger" : {
-        "channelId" : 1234,
-        "defaultGame" : "Example Game" #set to bool of False to not have one
+        "channelId" : 458748688504848396,
+        "defaultGame" : "False" #set to bool of False to not have one
     },
     "Viewer" : {
 
