4a02899f1cfa3251478d0686a2ae9f493179aa68
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2019-10-30 00:21:54 +0000

    Update for Viewer

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 #This is the Pycord Rewrite config file. This is used by both Messenger and Viewer
 settings = {
-    "token" : "",
-    "ExperimantalMode" : 0, #enables features that might not be working well
+    "token" : "BotToken",
+    "ExperimantalMode" : 1, #enables features that might not be working well
     "Messenger" : {
-        "channelId" : 458748688504848396,
-        "defaultGame" : "False" #set to bool of False to not have one
+        "channelId" : 458748688504848396, #Make sure its an int
+        "defaultGame" : "Example Game" #set to bool of False to not have one
     },
     "Viewer" : {
-
+        "ViewAll" : 0, #If set to 1, it will show messages in all channels (NOT RECOMMENDED)
+        "ChannelWhitelist" : [456091543669833728, 453226417782784000], #List of channel ids (ints) to show msgs from if ViewAll == 0
+        "TimeFormat" : 1 #Chooses between time formats 0-3
     }
 }
 
