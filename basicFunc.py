b981764d00c2b437ffe1f9b15191862eeb1013ba
RealistikDash <36131887+RealistikDash@users.noreply.github.com>
2019-11-17 20:31:34 +0000

    Added French + German + Polish translation
    Dragoofficial: French
    TheRealCed: German
    RealistikDash: Polish

--- basicFunc.py
+++ basicFunc.py
@@ -136,6 +136,7 @@ class LText:
         ExitExplain = "Logs out and quits Pycord"
 
         StartupLoading = "Loading Pycord Messenger..."
+        StartupLoadingViewer = "Loading Pycord Viewer..."
         BetaNotice = "This is still a work in progress so it shall not represent the final product."
         
         LoadingDone = " Loading finished!\nLogged into Discord via"
@@ -149,14 +150,95 @@ class LText:
 
         ErrorMessage = "An error has occured with Pycord!\nError: {}"
 
+        #Cotribution Table
+        ContributionThanks = "Thanks to all these people who contributed to the project"
+        RealistikContrib = "Coding, Polish Translation"
+        DragoContrib = "French translation"
+        CedContrib = "German translation"
+
     if settings["lang"].lower() == "fr":
-        #French lang. copy and paste the above and translate
-        pass
+        CommandIntro = "La liste de commandes disponibles sont"
+        HelpExplain = "Montre ce message"
+        FileExplain = "Envoi un fichier situé à un chemin spécifique"
+        GameExplain = "Définit un message de jeu personnalisé (utilise 'off' pour aucun)"
+        ExitExplain = "Déconnecte et quitte Pycord"
+
+        StartupLoading = "Chargement de Pycord Messagerie..."
+        StartupLoadingViewer = "Chargement de Pycord Visionneuse..."
+        BetaNotice = "Ceci est encore un travaux en cours donc ce ne devrait pas représenter le produit final."
+        
+        LoadingDone = " Chargement terminé!\nConnecté sur Discord avec"
+        GameChangingError = "Changement de jeu: {}"
+        ModuleError = "\nErreur criticale rencontrée durant l'importation de modules criticals! !"
+        ConfigInvalid = "Échec de la verification du fichier de configuration! Assurez-vous que le fichier existe et qu'il correspond à la plus nouvelle version disponible sur GitHub"
+        EmptyMessageError = "Impossible d'envoyer des messages vide!"
+        FileSendingError = "Envoi du fichier: {}"
+        MessageSendingError = "Envoi du message: {}"
+        LoginError = "Connection : {}"
+
+        ErrorMessage = "Une erreur est survenue avec Pycord!\nErreur: {}"
+
+        #Cotribution Table
+        ContributionThanks = "Merci à tous ces personnes qui ont contribué à ce projet"
+        RealistikContrib = "Codage, Traduction Polonaise"
+        DragoContrib = "Traduction Française"
+        CedContrib = "Traduction Allemande"
 
     if settings["lang"].lower() == "de":
-        #German lang. copy and paste the above and translate
-        pass
+        CommandIntro = "Die Liste der verfügbaren Befehle ist"
+        HelpExplain = "Zeigt diese Nachricht"
+        FileExplain = "Sendet eine Datei, die sich an einem bestimmten Pfad befindet"
+        GameExplain = "Setzt einen benutzerdefinierten Spiele Status (Benutze off für keinen)"
+        ExitExplain = "Abmelden und das verlassen von Pycord"
 
+        StartupLoading = "Laden des Pycord Messenger..."
+        StartupLoadingViewer = "Laden des Pycord Viewer..."
+        BetaNotice = "Dies ist noch in Arbeit, so dass es nicht das Endprodukt darstellen soll."
+        
+        LoadingDone = "Laden abgeschlossen!\nEingeloggt in Discord über"
+        GameChangingError = "Spieländerung: {}"
+        ModuleError = "\nEin kritischer Fehler ist beim Importieren von kritischen modulen aufgetreten!"
+        ConfigInvalid = "Überprüfung der Konfigurationsdatei fehlgeschlagen! Stellen sie sicher, das sie existiet und es entspricht der neusten Version auf GitHub"
+        EmptyMessageError = "leere Nachrichten können nicht gesendet werden!"
+        FileSendingError = "Datei senden: {}"
+        MessageSendingError = "Nachricht senden: {}"
+        LoginError = "Anmelden : {}"
+
+        ErrorMessage = "Ein Fehler ist mit Pycord aufgetreten!\nFehler: {}"
+
+        #Cotribution Table
+        ContributionThanks = "Danke an all die Leute die beim Projekt mit beigetragen haben"
+        RealistikContrib = "Programmierung, Polnische Übersetzung"
+        DragoContrib = "Französische Übersetzung"
+        CedContrib = "Deutsche Übersetzung"
+
+    if settings["lang"].lower() == "pl":
+        #Polish commands
+        CommandIntro = "Lista dostępnych komend jest"
+        HelpExplain = "Pokazuje tą wiadomość"
+        FileExplain = "Wysyła plik zlokalizowany w danej ścieżce"
+        GameExplain = "Ustawia wiadomość 'grania' na podaną przez użytkownika (ustaw na off aby wyłączyć)"
+        ExitExplain = "Wylogowuje się z Pycord"
+
+        StartupLoading = "Ładowanie Pycord Messenger..."
+        BetaNotice = "Jest to wciąż praca w toku, więc nie będzie reprezentować produktu końcowego."
+        
+        LoadingDone = " Ładowanie zakończone!\nZalogowany do Discord przez"
+        GameChangingError = "Zmiana gry: {}"
+        ModuleError = "\nWystąpił błąd krytyczny podczas importu krytycznych modułów!"
+        ConfigInvalid = "Nie udało się zweryfikować pliku konfiguracyjnego! Upewnij się, że istnieje i odpowiada najnowszej wersji dostępnej na GitHub!"
+        EmptyMessageError = "Nie można wysłać pustych wiadomości!"
+        FileSendingError = "Wysyłanie pliku: {}"
+        MessageSendingError = "Wysyłanie wiadomiści: {}"
+        LoginError = "Logowanie się : {}"
+
+        ErrorMessage = "Wystąpił błąd w Pycord!\nBłąd: {}"
+
+        #Cotribution Table
+        ContributionThanks = "Dziękuję wszystkim osobom, które odegrały rolę w tym projekcie"
+        RealistikContrib = "Programowanie, Polskie tłumaczenie"
+        DragoContrib = "Francuskie tłumaczenie"
+        CedContrib = "Niemieckie tłumaczenie"
 
 def ErrorMessage(message):
     """Template for error messages (not finished)"""
@@ -165,3 +247,12 @@ def ErrorMessage(message):
 async def AsyncErrorMessage(message):
     """Template for error messages. But guess what? It's asyncronous (not finished)"""
     print(LText.ErrorMessage.format(message))
+
+def Contributors():
+    """Displays contributors"""
+    print("----------------------------------------------------------")
+    print("{}".format(LText.ContributionThanks))
+    print("RealistikDash        {}".format(LText.RealistikContrib))
+    print("Dragoofficial        {}".format(LText.DragoContrib))
+    print("TheRealCed           {}".format(LText.CedContrib))
+    print("----------------------------------------------------------")
